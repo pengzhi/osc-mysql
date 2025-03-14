7979080c1f5ef342273fcde385b23bcfd1c3068e
Peng <pengzhiq@gmail.com>
2025-03-14 11:19:41 -0400

    Update mysql connection to use username/password

--- lambda_function.py
+++ lambda_function.py
@@ -8,6 +8,7 @@ proxy_host_name = os.environ['PROXY_HOST_NAME']
 port = int(os.environ['PORT'])
 db_name = os.environ['DB_NAME']
 db_user_name = os.environ['DB_USER_NAME']
+db_password = os.environ['DB_PASSWORD']
 aws_region = os.environ['AWS_REGION']
 
 
@@ -23,15 +24,14 @@ def get_auth_token():
     return token
 
 def lambda_handler(event, context):
-    token = get_auth_token()
+    # token = get_auth_token()
     try:
         connection = pymysql.connect(
             host=proxy_host_name,
             user=db_user_name,
-            password=token,
+            password=db_password,
             db=db_name,
-            port=port,
-            ssl={'ca': 'Amazon RDS'}  # Ensure you have the CA bundle for SSL connection
+            port=port
         )
         
         with connection.cursor() as cursor:
@@ -43,3 +43,4 @@ def lambda_handler(event, context):
     except Exception as e:
         return (f"Error: {str(e)}")  # Return an error message if an exception occurs 
     
+
